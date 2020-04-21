ff0aeb7223b7463a223ef17e1da5d6b1cee4861c
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2020-04-21 02:19:53 -0400

    Comentarios en readme

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,22 @@ class Person < ActiveRecord::Base
   belongs_to :person
   has_many :messages
 
+  has_many :events
+  has_many :persons
+  has_one :message_inbox
+  has_many :messages, through::message_inbox
+  belongs_to :organization
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
+  validates :email, confirmation: { case_sensitive: false }, format: { with: /\A(\S+)@(.+)\.(\S+)\z/}
+  validates :username, presence: true
+  validates :adress, presence: true
+  validates :email, confirmation: true
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+git
 
 
 end
