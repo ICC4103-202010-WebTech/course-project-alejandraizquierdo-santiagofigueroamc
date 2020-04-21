703525639b742dbb22423dc60ed11bca26a529c9
toor <webtech>
2020-04-21 01:09:51 -0400

    associaciones hechas

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,16 @@
 class Organization < ActiveRecord::Base
+  has_many :events
+  has_many :persons
+  has_one :message_inbox
+  has_many :messages, through::message_inbox
+  has_many :perssons
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
+  validates :username, presence: true
+  validates :adress, presence: true
+  validates :email, confirmation: true
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,19 @@
 class Person < ActiveRecord::Base
+  has_many :events
+  has_many :persons
+  has_one :message_inbox
+  has_many :messages, through::message_inbox
+  belongs_to :organization
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
+  validates :username, presence: true
+  validates :adress, presence: true
+  validates :email, confirmation: true
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+git
+
+
 end
