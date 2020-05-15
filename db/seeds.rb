36da0e248da2b2ce68a8924562ad51d98a1aebad
Santiago Figueroa <37884952+SantiagoFigueroaMc@users.noreply.github.com>
2020-05-15 19:31:01 -0400

    seed para personas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users (Person)'s
+Person.create(
+    first_name: 'Alejandra',
+    last_name: 'Izquierdo',
+    username: 'aleIzquierdo',
+    email: 'ale@gmail.com',
+    password: '1234',
+    biography: "This is Ale's biography",
+    address: "sesame street 1221")
+
+Person.create(
+    first_name: 'Santiago',
+    last_name: 'Figueroa',
+    username: 'sfigueroa',
+    email: 'santiago@gmail.com',
+    password: '1111',
+    biography: "This is Santiago's biography",
+    address: "sesame street 2254")
+
+# Events
+
+
+# Organizations
