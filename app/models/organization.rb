e6f420e1a400c6d3a44a417ed2bddf16d944743b
alejandraIzquierdo <aizquierdo1@miuandes.cl>
2020-05-15 20:25:04 -0400

    organization fixed

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,14 +1,34 @@
 class Organization < ApplicationRecord
-  has_many :events
-  has_many :persons
+  belongs_to :person
   has_many :messages
-  has_many :persons
+  has_many :posts
+
+  has_many :events
+  has_many :people
   validates :name, presence: true
+  validates :last_name, presence: true
   validates :password, presence: true
   validates :email, presence: true
+  validates :email, confirmation: { case_sensitive: false }, format: { with: /\A(\S+)@(.+)\.(\S+)\z/}
   validates :username, presence: true
   validates :address, presence: true
   validates :email, confirmation: true
   validates :email, uniqueness: true
   validates :username, uniqueness: true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
